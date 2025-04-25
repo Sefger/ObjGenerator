6805e95dd062a26735ae975867d82438ccbd033d
unknown <sefer2006@gmail.com>
2025-04-25 20:53:26 +0300

    Removed unnecessary comments

--- ObjGenerator.cpp
+++ ObjGenerator.cpp
@@ -27,7 +27,7 @@ bool isNumber(const std::string& s) {
     return !s.empty() && std::find_if_not(s.begin(),
         s.end(), [](char c) { return std::isdigit(c) || c == '.'; }) == s.end();
 }
-//может стоит убрать эту функцию
+
 bool checkIsDouble(std::string str) {
     try
     {
@@ -45,97 +45,69 @@ bool checkIsDouble(std::string str) {
         return false;
     }
 }
-//можно будет сделать чтобы generatePolygon использовал generateTriangle, и так далее
-// методы для генерации
+
 void generateTriangle(std::ofstream& file, std::vector<Vertex> vec) {
-    ObjStreamWriter* writer = new ObjStreamWriter(file);
+    ObjStreamWriter writer(file);
+    
     for (Vertex v : vec) {
-        /*myFile << "v " << v.getX() << " " << v.getY() << " " << v.getZ()<<"\n";*/
-        writer->addVertex(v.getX(), v.getY(), v.getZ());
+        
+        writer.addVertex(v.getX(), v.getY(), v.getZ());
     }
-    //file << "f" << " 1" << " 2" << " 3\n" << std::endl;
-    writer->addFacet(writer->getSize(), writer->getSize() - 1, writer->getSize() - 2);
+    
+    writer.addFacet(writer.getSize(), writer.getSize() - 1, writer.getSize() - 2);
 }
 void generateTriangle(std::ofstream& file, double x, double y, double z) {
-    ObjStreamWriter* writer = new ObjStreamWriter(file);
-    writer->addVertex(x, y, z);
-    writer->addFacet(writer->getSize() , writer->getSize() - 1, writer->getSize() - 2);
-   /* file << "v " << x << " " << y << " " << z;
-    myFile << "f" << " 1" << " 2" << " 3" << std::endl;*/
+    ObjStreamWriter writer(file);
+    writer.addVertex(x, y, z);
+    writer.addFacet(writer.getSize() , writer.getSize() - 1, writer.getSize() - 2);
+   
 }
 void generatePolygon(std::ofstream& file, double length, double width) {
-    ObjStreamWriter* writer = new ObjStreamWriter(file);
-    writer->addVertex(0, 0, 0);//-4
-    writer->addVertex(length, 0, 0);//-3
-    writer->addVertex(length, width, 0);//-2
-    writer->addVertex(0, width, 0);//-1
-    writer->addFacet(writer->getSize() - 3, writer->getSize() - 2, writer->getSize() - 1);
-    writer->addFacet(writer->getSize() - 3, writer->getSize() - 1, writer->getSize());
-   /* myFile << "v " << 0.0 << " " << 0.0 << " " << 0.0 << std::endl;
-    myFile << "v " << length << " " << 0.0 << " " << 0.0 << std::endl;
-    myFile << "v " << length << " " << width << " " << 0.0 << std::endl;
-    myFile << "v " << 0.0 << " " << width << " " << 0.0 << std::endl;
-    myFile << "f 1 2 3" << std::endl;
-    myFile << "f 1 3 4" << std::endl;*/
+    ObjStreamWriter writer(file);
+    writer.addVertex(0, 0, 0);//-4
+    writer.addVertex(length, 0, 0);//-3
+    writer.addVertex(length, width, 0);//-2
+    writer.addVertex(0, width, 0);//-1
+    writer.addFacet(writer.getSize() - 3, writer.getSize() - 2, writer.getSize() - 1);
+    writer.addFacet(writer.getSize() - 3, writer.getSize() - 1, writer.getSize());
 };
 void generateParallelepiped(std::ofstream& file, double length, double width, double height) {
-    ObjStreamWriter* writer = new ObjStreamWriter(file);
-    writer->addVertex(0, 0, 0);// 1 - 8
-    writer->addVertex(length, 0, 0);// 2 - 7
-    writer->addVertex(length, width, 0);// 3 - 6
-    writer->addVertex(0, width, 0);// 4 - 5
-    writer->addVertex(0, 0, height);//5 - 4
+    ObjStreamWriter writer(file);
+    writer.addVertex(0, 0, 0);
+    writer.addVertex(length, 0, 0);
+    writer.addVertex(length, width, 0);
+    writer.addVertex(0, width, 0);
+    writer.addVertex(0, 0, height);
 
-    writer->addVertex(length, 0, height);// 6 -3
-    writer->addVertex(length, width, height);// 7 -2
-    writer->addVertex(0, width, height);// 8 -1
-    /*myFile << "v " << 0.0 << " " << 0.0 << " " << 0.0 << std::endl;
-    myFile << "v " << length << " " << 0.0 << " " << 0.0 << std::endl;
-    myFile << "v " << length << " " << width << " " << 0.0 << std::endl;
-    myFile << "v " << 0.0 << " " << width << " " << 0.0 << std::endl;
-
-    myFile << "v " << 0.0 << " " << 0.0 << " " << height << std::endl;
-    myFile << "v " << length << " " << 0.0 << " " << height << std::endl;
-    myFile << "v " << length << " " << width << " " << height << std::endl;
-    myFile << "v " << 0.0 << " " << width << " " << height << std::endl;*/
-
-    writer->addFacet(writer->getSize() - 7, writer->getSize() - 6, writer->getSize() - 5);
-    writer->addFacet(writer->getSize() - 7, writer->getSize() - 5, writer->getSize() - 4);
-    //myFile << "f 1 2 3" << std::endl;
-    //myFile << "f 1 3 4" << std::endl;
-    
-
-    writer->addFacet(writer->getSize() - 3, writer->getSize() - 2, writer->getSize()-1);
-    writer->addFacet(writer->getSize() - 3, writer->getSize() - 1, writer->getSize());
-    ////// Верхняя грань
-    ////myFile << "f 5 6 7" << std::endl;
-    ////myFile << "f 5 7 8" << std::endl;
-    
-    
-    writer->addFacet(writer->getSize() - 7, writer->getSize() - 3, writer->getSize() - 2);
+    writer.addVertex(length, 0, height);
+    writer.addVertex(length, width, height);
+    writer.addVertex(0, width, height);
   
-    writer->addFacet(writer->getSize() - 7, writer->getSize() - 2, writer->getSize()-6);
 
-    ////// Передняя грань
-    ////myFile << "f 1 5 6" << std::endl;
-    ////myFile << "f 1 6 2" << std::endl;
-    //
-    writer->addFacet(writer->getSize() - 5, writer->getSize() - 1, writer->getSize());
-    writer->addFacet(writer->getSize() - 5, writer->getSize(), writer->getSize() - 4);
-    ////// Задняя грань
-    ////myFile << "f 3 7 8" << std::endl;
-    ////myFile << "f 3 8 4" << std::endl;
-    writer->addFacet(writer->getSize() - 7, writer->getSize() - 3, writer->getSize());
-    writer->addFacet(writer->getSize() - 7, writer->getSize(), writer->getSize() - 4);
-    ////// Левая грань
-    ////myFile << "f 1 5 8" << std::endl;
-    ////myFile << "f 1 8 4" << std::endl;
+    writer.addFacet(writer.getSize() - 7, writer.getSize() - 6, writer.getSize() - 5);
+    writer.addFacet(writer.getSize() - 7, writer.getSize() - 5, writer.getSize() - 4);
+ 
 
-    writer->addFacet(writer->getSize() - 6, writer->getSize() - 2, writer->getSize() - 1);
-    writer->addFacet(writer->getSize() - 6, writer->getSize() - 1, writer->getSize() - 5);
-    //// Правая грань
-    //myFile << "f 2 6 7" << std::endl;
-    //myFile << "f 2 7 3" << std::endl;
+    writer.addFacet(writer.getSize() - 3, writer.getSize() - 2, writer.getSize()-1);
+    writer.addFacet(writer.getSize() - 3, writer.getSize() - 1, writer.getSize());
+
+    
+    
+    writer.addFacet(writer.getSize() - 7, writer.getSize() - 3, writer.getSize() - 2);
+  
+    writer.addFacet(writer.getSize() - 7, writer.getSize() - 2, writer.getSize()-6);
+
+
+    writer.addFacet(writer.getSize() - 5, writer.getSize() - 1, writer.getSize());
+    writer.addFacet(writer.getSize() - 5, writer.getSize(), writer.getSize() - 4);
+   
+    writer.addFacet(writer.getSize() - 7, writer.getSize() - 3, writer.getSize());
+    writer.addFacet(writer.getSize() - 7, writer.getSize(), writer.getSize() - 4);
+  
+
+    writer.addFacet(writer.getSize() - 6, writer.getSize() - 2, writer.getSize() - 1);
+    writer.addFacet(writer.getSize() - 6, writer.getSize() - 1, writer.getSize() - 5);
+    
 
 }
 void generateCube(std::ofstream& myFile, double side) {
@@ -145,40 +117,32 @@ void generatePiramid(std::ofstream& file, double length, double width, double he
     // ширина по x 
     // длина по y
     //пока пирамида имеет квадратное основание
-    ObjStreamWriter* writer = new ObjStreamWriter(file);
-    writer->addVertex(0, 0, 0);// 1 - 5
-    writer->addVertex(0, length, 0);// 2 - 4
-    writer->addVertex(width, 0, 0);// 3 - 3
-    writer->addVertex(width, length, 0);// 4 - 2
-    writer->addVertex(width / 2, length / 2, height);// 5 - 1
+    ObjStreamWriter writer(file);
+    writer.addVertex(0, 0, 0);// 1 - 5
+    writer.addVertex(0, length, 0);// 2 - 4
+    writer.addVertex(width, 0, 0);// 3 - 3
+    writer.addVertex(width, length, 0);// 4 - 2
+    writer.addVertex(width / 2, length / 2, height);// 5 - 1
 
-    /*myFile << "v " << 0.0 << " " << 0.0 << " " << 0.0 << std::endl;
-    myFile << "v " << 0.0 << " " <<  length << " " << 0.0 << std::endl;
-    myFile << "v " << width << " " << 0.0 << " " << 0.0 << std::endl;
-    myFile << "v " << width << " " << length << " " << 0.0 << std::endl;
-    myFile << "v " << width/2 << " " << length/2 << " " << height << std::endl;*/
-    //основания
+    
 
-    writer->addFacet(writer->getSize() - 4, writer->getSize() - 3, writer->getSize() - 2);
-    writer->addFacet(writer->getSize() - 3, writer->getSize() - 2, writer->getSize() - 1);
-    //myFile << "f 1 2 3 "<<std::endl;
-    //myFile << "f 2 3 4 "<<std::endl;
+    writer.addFacet(writer.getSize() - 4, writer.getSize() - 3, writer.getSize() - 2);
+    writer.addFacet(writer.getSize() - 3, writer.getSize() - 2, writer.getSize() - 1);
+    
     ////стороны
-    writer->addFacet(writer->getSize() - 4, writer->getSize() - 3, writer->getSize());
-    writer->addFacet(writer->getSize() - 3, writer->getSize() - 1, writer->getSize());
-    ////myFile << "f 1 2 5"<<std::endl;
-    ////myFile << "f 2 4 5"<<std::endl;
-    writer->addFacet(writer->getSize() - 2, writer->getSize() - 1, writer->getSize() );
-    writer->addFacet(writer->getSize() - 4, writer->getSize() -2, writer->getSize() );
-    //myFile << "f 3 4 5"<<std::endl;
-    //myFile << "f 1 3 5"<<std::endl;
+    writer.addFacet(writer.getSize() - 4, writer.getSize() - 3, writer.getSize());
+    writer.addFacet(writer.getSize() - 3, writer.getSize() - 1, writer.getSize());
+   
+    writer.addFacet(writer.getSize() - 2, writer.getSize() - 1, writer.getSize() );
+    writer.addFacet(writer.getSize() - 4, writer.getSize() -2, writer.getSize() );
+
 }
 void generateTruncatedPiramid(std::ofstream& file, double length, double width, double height, double newHeight) {
     if (newHeight >= height) {
         std::cout << "Invalid input";
         return;
     }
-    ObjStreamWriter* writer = new ObjStreamWriter(file);
+    ObjStreamWriter writer(file);
 
     // Рассчитываем параметры усечения
     double ratio = newHeight / height;
@@ -198,68 +162,47 @@ void generateTruncatedPiramid(std::ofstream& file, double length, double width, 
     double v6x = topWidth / 2, v6y = -topLength / 2, v6z = newHeight;  // 6
     double v7x = topWidth / 2, v7y = topLength / 2, v7z = newHeight;  // 7
     double v8x = -topWidth / 2, v8y = topLength / 2, v8z = newHeight;  // 8
-    writer->addVertex(v1x, v1y, v1z);
-    writer->addVertex(v2x, v2y, v2z);
-    writer->addVertex(v3x, v3y, v3z);
-    writer->addVertex(v4x, v4y, v4z);
+    writer.addVertex(v1x, v1y, v1z);
+    writer.addVertex(v2x, v2y, v2z);
+    writer.addVertex(v3x, v3y, v3z);
+    writer.addVertex(v4x, v4y, v4z);
 
-    writer->addVertex(v5x, v5y, v5z);
-    writer->addVertex(v6x, v6y, v6z);
-    writer->addVertex(v7x, v7y, v7z);
-    writer->addVertex(v8x, v8y, v8z);
-    // Записываем вершины
-   
-
-    // Нижнее основание
-   
-    //myFile << "v " << v1x << " " << v1y << " " << v1z << "\n";
-    //myFile << "v " << v2x << " " << v2y << " " << v2z << "\n";
-    //myFile << "v " << v3x << " " << v3y << " " << v3z << "\n";
-    //myFile << "v " << v4x << " " << v4y << " " << v4z << "\n";
-
-    //// Верхнее основание
-    //myFile << "v " << v5x << " " << v5y << " " << v5z << "\n";
-    //myFile << "v " << v6x << " " << v6y << " " << v6z << "\n";
-    //myFile << "v " << v7x << " " << v7y << " " << v7z << "\n";
-    //myFile << "v " << v8x << " " << v8y << " " << v8z << "\n";
-
-    // Грани (все треугольники)
+    writer.addVertex(v5x, v5y, v5z);
+    writer.addVertex(v6x, v6y, v6z);
+    writer.addVertex(v7x, v7y, v7z);
+    writer.addVertex(v8x, v8y, v8z);
+    
 
 
     // Нижнее основание (2 треугольника)
-    writer->addFacet(writer->getSize() - 7, writer->getSize() - 6, writer->getSize() - 5);
-    writer->addFacet(writer->getSize() - 7, writer->getSize() - 5, writer->getSize() - 4);
-    /*myFile << "f 1 2 3\n";
-    myFile << "f 1 3 4\n";*/
+    writer.addFacet(writer.getSize() - 7, writer.getSize() - 6, writer.getSize() - 5);
+    writer.addFacet(writer.getSize() - 7, writer.getSize() - 5, writer.getSize() - 4);
+
 
     // Верхнее основание (2 треугольника)
-    writer->addFacet(writer->getSize() - 3, writer->getSize() - 1, writer->getSize() - 2);
-    writer->addFacet(writer->getSize()-3, writer->getSize(), writer->getSize()-1);
+    writer.addFacet(writer.getSize() - 3, writer.getSize() - 1, writer.getSize() - 2);
+    writer.addFacet(writer.getSize()-3, writer.getSize(), writer.getSize()-1);
    /* myFile << "f 5 7 6\n";
     myFile << "f 5 8 7\n";*/
 
     // Боковые грани (8 треугольников)
-    writer->addFacet(writer->getSize() - 7, writer->getSize() - 6, writer->getSize() - 2);
-    writer->addFacet(writer->getSize() - 7, writer->getSize() - 2, writer->getSize() - 3);
-    //myFile << "f 1 2 6\n";  // Передняя сторона 1
-    //myFile << "f 1 6 5\n";  // Передняя сторона 2
+    writer.addFacet(writer.getSize() - 7, writer.getSize() - 6, writer.getSize() - 2);
+    writer.addFacet(writer.getSize() - 7, writer.getSize() - 2, writer.getSize() - 3);
+   
 
-    writer->addFacet(writer->getSize() - 6, writer->getSize() - 5, writer->getSize() - 1);
-    writer->addFacet(writer->getSize() - 6, writer->getSize() - 1, writer->getSize() - 2);
-    //myFile << "f 2 3 7\n";  // Правая сторона 1
-    //myFile << "f 2 7 6\n";  // Правая сторона 2
+    writer.addFacet(writer.getSize() - 6, writer.getSize() - 5, writer.getSize() - 1);
+    writer.addFacet(writer.getSize() - 6, writer.getSize() - 1, writer.getSize() - 2);
+  
 
-    writer->addFacet(writer->getSize() - 5, writer->getSize() - 4, writer->getSize());
+    writer.addFacet(writer.getSize() - 5, writer.getSize() - 4, writer.getSize());
 
-    writer->addFacet(writer->getSize() - 5, writer->getSize() , writer->getSize() - 1);
+    writer.addFacet(writer.getSize() - 5, writer.getSize() , writer.getSize() - 1);
 
-    //myFile << "f 3 4 8\n";  // Задняя сторона 1
-    //myFile << "f 3 8 7\n";  // Задняя сторона 2
 
-    writer->addFacet(writer->getSize() - 4, writer->getSize() - 7, writer->getSize() - 3);
-    writer->addFacet(writer->getSize() - 4, writer->getSize() - 3, writer->getSize());
-    //myFile << "f 4 1 5\n";  // Левая сторона 1
-    //myFile << "f 4 5 8\n";
+
+    writer.addFacet(writer.getSize() - 4, writer.getSize() - 7, writer.getSize() - 3);
+    writer.addFacet(writer.getSize() - 4, writer.getSize() - 3, writer.getSize());
+ 
 
 }
 //разговоры с пользователем
@@ -301,7 +244,7 @@ void createTriangle(std::ofstream& myFile) {
             x = std::stod(str1);
             y = std::stod(str2);
             z = std::stod(str3);
-            //здесь вроде должен быть new, но компилятор жаловался
+           
             temp.push_back(Vertex(x, y, z));
             count++;
         }
@@ -383,17 +326,10 @@ int main() {
         break;
 
     case 6:
+        //тестировал
         createTruncatedPiramid(myFile);
         break;
     }
 
     myFile.close();
 }
-//int main() {
-//    std::ofstream myFile("obj2.obj");
-//    ObjStreamWriter temp(myFile);
-//    temp.addVertex(0, 0, 0);
-//    temp.addVertex(1, 1, 1);
-//    temp.addVertex(0, 0, 1);
-//    temp.addFacet(1, 2, 3);
-//}
